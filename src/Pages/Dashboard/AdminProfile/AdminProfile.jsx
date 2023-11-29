efa8f571460a120ea748a92bec320565911651f7
Syed Ashiq <imsyedashiq@gmail.com>
2023-11-29 11:09:31 +0600

    pie Chart added

--- src/Pages/Dashboard/AdminProfile/AdminProfile.jsx
+++ src/Pages/Dashboard/AdminProfile/AdminProfile.jsx
@@ -4,11 +4,14 @@ import useAxiosSecure from '../../../Hooks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
 import { RiArticleFill } from 'react-icons/ri';
 import { FaComment, FaUser } from 'react-icons/fa';
+import { PieChart, Pie, Cell } from 'recharts';
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 const AdminProfile = () => {
   const { user } = useContext(AuthContext);
   const axiosSecure = useAxiosSecure();
-  const { data: stats = {} } = useQuery({
+  const { data: stats } = useQuery({
     queryKey: ['stats'],
     queryFn: async () => {
       const res = await axiosSecure.get('/admin-stats');
@@ -17,6 +20,40 @@ const AdminProfile = () => {
   });
 
   console.log(stats);
+
+  //   custom shape for pie chart
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+  const pieChartData = [
+    { name: 'Post', value: stats.posts },
+    { name: 'User', value: stats.users },
+    { name: 'Comment', value: stats.comments },
+  ];
+
   return (
     <div className="text-center ">
       <div className="relative w-40 mx-auto mt-10">
@@ -50,6 +87,24 @@ const AdminProfile = () => {
           </div>
         </div>
       </div>
+      {/* Pie chart */}
+
+      <PieChart width={400} height={400} className="mx-auto  ">
+        <Pie
+          data={pieChartData}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {pieChartData.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
     </div>
   );
 };
